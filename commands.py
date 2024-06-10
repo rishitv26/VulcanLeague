a023e1b78d753b3483bb7c7ea088566490bf5fec
rishitv26 <varshneyrishit5@gmail.com>
2024-06-09 23:43:45 -0400

    Ready for use.

--- commands.py
+++ commands.py
@@ -51,11 +51,11 @@ def main():
         elif cmd == "exit":
             util.exit_routine()
         elif cmd == "manual":
-            print("1. Add all segments using add-segment command for ink evaluation.")
-            print("2. Remove any segments if neccessary.")
-            print("3. Choose between training data combinations of the following: 1; 1,2; 1,2,3. DO NOT PUT ANYTHING ELSE")
-            print("4. When chosen, set it as value for training_data setting using change-setting")
-            print("5. train the model by train command")
+            print("1. Choose between training data combinations of the following: 1; 1,2; 1,2,3. DO NOT PUT ANYTHING ELSE")
+            print("2. When chosen, set it as value for training_data setting using change-setting")
+            print("3. train the model by train command")
+            print("4. Add all segments using add-segment command for ink evaluation.")
+            print("5. Remove any segments if neccessary.")
             print("6. run the ink evaluation by eval command")
             print("7. snaphot output and save it useful")
             print("8. Run it through letter detector. (TODO)")
