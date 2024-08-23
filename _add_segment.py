fda2df77cba1caafdf72a0af2aa5d32225734e6e
rishitv26 <varshneyrishit5@gmail.com>
2024-08-23 11:52:38 -0400

    fixed bugs with rm and add segment

--- _add_segment.py
+++ _add_segment.py
@@ -8,7 +8,7 @@ def main(cmd_list: list[str], config: Config, detector):
         illegal_argument(2)
     else:
         try:
-            destination_folder = os.path.join(config.get("base_path") + '/data/vesuvius-challenge-ink-detection/test/', os.path.basename(cmd_list[1]))
+            destination_folder = os.path.join(os.path.join(config.get("base_path"), '/data/vesuvius-challenge-ink-detection/test/'), os.path.basename(cmd_list[1]))
             shutil.copytree(cmd_list[1], destination_folder)
         except FileExistsError:
             segment_exists()
