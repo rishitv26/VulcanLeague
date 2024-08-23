78c34bd1f86b6670f86ef36dab1dc2ae2e26331d
rishitv26 <varshneyrishit5@gmail.com>
2024-08-23 11:48:57 -0400

    testing

--- _rm_segment.py
+++ _rm_segment.py
@@ -7,6 +7,7 @@ def main(cmd_list: list[str], config: Config, detector):
         illegal_argument(2)
     else:
         try:
+            print(config.get('base_path') + 'data/vesuvius-challenge-ink-detection/test/' + cmd_list[1])
             shutil.rmtree(config.get('base_path') + 'data/vesuvius-challenge-ink-detection/test/' + cmd_list[1])
         except FileNotFoundError:
             segment_doesnt_exist()
